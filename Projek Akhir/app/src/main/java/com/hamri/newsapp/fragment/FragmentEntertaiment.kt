4b2920ebd94dd84a800a35c181e3e6394054b05b
hamriwahyu <82073879+hamriwahyu@users.noreply.github.com>
2021-06-02 12:29:14 +0800

    .

--- Projek Akhir/app/src/main/java/com/hamri/newsapp/fragment/FragmentEntertaiment.kt
+++ Projek Akhir/app/src/main/java/com/hamri/newsapp/fragment/FragmentEntertaiment.kt
@@ -20,15 +20,6 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
 
-/**
- * Created by Azhar Rivaldi on 10-04-2021
- * Github : https://github.com/AzharRivaldi
- * Linkedin : https://www.linkedin.com/in/azhar-rivaldi
- * Instagram : https://www.instagram.com/azhardvls_
- * Twitter : https://twitter.com/azharrvldi_
- * Youtube Channel : https://bit.ly/2PJMowZ
- */
-
 class FragmentEntertaiment : Fragment() {
 
     companion object {
